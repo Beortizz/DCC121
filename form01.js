5b19c68684fda6bad3aab518ed71fb3f5f9c32b7
Beortizz <beortizz@gmail.com>
2022-07-19 20:47:57 -0300

    criando novas divs

--- form01.js
+++ form01.js
@@ -1,13 +1,21 @@
 function criaElemento(cor, largura, altura)
 {
     const eNovo = document.createElement("div");
-    eNovo.textContent = `${cor} ${altura} ${largura}`;
+    eNovo.textContent = `${cor} ${largura} ${altura}`;
+    eNovo.style = `color: ${cor}`;
     document.body.appendChild(eNovo);
 
 }
 
-criaElemento(
-    document.exm01.cor.value,
-    document.exm01.altura.value,
-    document.exm01.largura.value
-)
+document.exm01.addEventListener("submit",submitListener);
+function submitListener(event)
+{
+    event.preventDefault();
+    criaElemento(
+        document.exm01.cor.value,
+        Number(document.exm01.altura.value),
+        Number(document.exm01.largura.value)
+    )
+    document.exm01.reset();
+}
+
